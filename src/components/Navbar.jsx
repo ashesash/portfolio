e58830bcae1492c0ebcae3c6d20aa78832110a5e
ashesash <aishwarya.s@live.com>
2023-02-15 17:47:31 +1100

    setup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,36 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons';
 
 const Navbar = () => {
     return (
         <div className='fixed w-full h-20 shadow-xl z-[100]'>
             <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
-                <Image
-                    src="/public/assets/as.svg"
-                    alt="/"
-                    width='125'
-                    height='125'
-                />
+            <Image src="/../public/assets/as.svg" alt="logo" width='100' height='100'/>
                 <div>
-                    <ul>
+                    <ul className='hidden md:flex'>
                         <Link href='/'>
-                            <li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
-                            <li className='ml-10 text-sm uppercase hover:border-b'>About</li>
-                            <li className='ml-10 text-sm uppercase hover:border-b'>Skills</li>
-                            <li className='ml-10 text-sm uppercase hover:border-b'>Project</li>
-                            <li className='ml-10 text-sm uppercase hover:border-b'>Contact</li>
+                        <li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
                         </Link>
+                        <Link href='/'>
+                        <li className='ml-10 text-sm uppercase hover:border-b'>About</li>
+                        </Link>
+                        <Link href='/'>
+                        <li className='ml-10 text-sm uppercase hover:border-b'>Skills</li>
+                        </Link>
+                        <Link href='/'>
+                            <li className='ml-10 text-sm uppercase hover:border-b'>Project</li>
+                        </Link>
+                        <Link href='/'>
+                        <li className='ml-10 text-sm uppercase hover:border-b'>Contact</li>
+                        </Link>                        
                     </ul>
                 </div>
+                {/* <div className='md:hidden'>
+                    <AiOutlineMenu size={25} />
+
+                </div> */}
             </div>
         </div>
     )
